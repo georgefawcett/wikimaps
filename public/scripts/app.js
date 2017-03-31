530805a06f00c29020beb5c7554cdf19bf4da76f
George Fawcett <georgealanfawcett@gmail.com>
2017-03-31 21:55:07 +0000

    Adds user dashboard

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,38 @@
-<<<<<<< HEAD
 
 
-=======
 $(document).ready(function(){
+
+
+  // make a .hover event
+    $('#marker_info').hover(
+      // mouse in
+      function () {
+        // first we need to know which <div class="marker"></div> we hovered
+        var index = $('.listlabel').index(this);
+        markers[index].setIcon(highlightedIcon());
+      },
+      // mouse out
+      function () {
+        // first we need to know which <div class="marker"></div> we hovered
+        var index = $('.listlabel').index(this);
+        markers[index].setIcon(normalIcon());
+      }
+
+    );
+
+      function normalIcon() {
+      return {
+        url: 'http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png'
+      };
+    }
+    function highlightedIcon() {
+      return {
+        url: 'http://steeplemedia.com/images/markers/markerGreen.png'
+      };
+    }
+
   $("#new-list").click(() => {
    window.location.href="/api/users/newlist";
   })
->>>>>>> c3af168441e80e19f8b7a72fe183826f9b0be373
+
 });
--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
+
+const cookieSession = require('cookie-session')
+app.use(cookieSession({
+  name: 'session',
+  keys: ['user_id'], //
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
@@ -41,7 +49,6 @@ app.use("/api/users", usersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   if (req.session.user_id) {
     knex
       .select("*")
@@ -119,6 +126,79 @@ app.get("/lists/:listID", (req, res) => {
 
 });
 
+
+
+
+app.get("/users/:userID", (req, res) => {
+
+let templateVars = {cookie: req.session.user};
+
+Promise.all([
+
+knex.select("*").from("users").where("id", req.params.userID).then((userResults) => {
+templateVars.user = userResults[0];
+}),
+
+knex.select("*").from("lists").where("user_id", req.params.userID).then((listResults) => {
+templateVars.list = listResults;
+}),
+
+// Join query to get title of lists user has contributed to
+knex('contributors')
+.join('lists', 'contributors.list_id', 'lists.id')
+.select ('contributors.list_id', 'lists.title')
+.where ('contributors.user_id', req.params.userID)
+.then((contResults) => {
+  templateVars.cont = contResults;
+}),
+
+// Double join query to get list & list author info of lists user has favourited
+knex('favourites')
+.join('lists', 'favourites.list_id', 'lists.id')
+.join('users', 'lists.user_id', 'users.id')
+.select ('favourites.list_id', 'lists.title', 'lists.description', 'users.id', 'users.name')
+.where ('favourites.user_id', req.params.userID)
+.then((favResults) => {
+  templateVars.fav = favResults;
+}),
+
+
+// Double join query of recent contributions and join query
+// of recent new lists for Activity Feed.
+// Ensure lists have public view or edit privacy settings
+// 0 = fully public; 1 = public view only; 2 = private
+
+knex('contributors')
+.join('lists', 'contributors.list_id', 'lists.id')
+.join('users', 'contributors.user_id', 'users.id')
+.select ('contributors.list_id', 'lists.title', 'users.name', 'users.id', 'contributors.date_created')
+.where('lists.privacy', 0).orWhere('lists.privacy', 1)
+.orderBy('contributors.date_created', 'desc')
+.limit(5)
+.then((publicContResults) => {
+  templateVars.publicCont = publicContResults;
+}),
+
+knex('lists')
+.join('users', 'lists.user_id', 'users.id')
+.select ('lists.id as listid', 'lists.title', 'lists.date_created', 'users.name', 'users.id')
+.where('lists.privacy', 0).orWhere('lists.privacy', 1)
+.orderBy('lists.date_created', 'desc')
+.limit(5)
+.then((publicListResults) => {
+  templateVars.publicList = publicListResults;
+}),
+
+
+]).then(() => {
+  res.render("users", templateVars);
+});
+
+
+});
+
+
+
 // Logout
 app.post("/logout", (req, res) => {
   req.session = null;
@@ -163,10 +243,6 @@ app.post("/register", (req, res) => {
 
 
 
-=======
-
-
->>>>>>> c3af168441e80e19f8b7a72fe183826f9b0be373
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
